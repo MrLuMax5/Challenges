f2493a1323064098020f9298be3f18123eccb71c
8voss <8voss@informatik.uni-hamburg.de>
2022-09-25 14:56:24 +0200

    Add DesignCircularQueue

--- Leetcode/src/main/java/com/vosseptum/leetcode/Main.java
+++ Leetcode/src/main/java/com/vosseptum/leetcode/Main.java
@@ -2,7 +2,7 @@ package com.vosseptum.leetcode;
 
 public class Main {
     public static void main(String[] args) {
-        LongestSubstringWithoutRepeatingCharacters solution = new LongestSubstringWithoutRepeatingCharacters();
-        System.out.println(solution.lengthOfLongestSubstring("tmmzuxtasdfghtmmzuxt"));
+        DesignCircularQueue myqueue = new DesignCircularQueue(3);
+        System.out.println(myqueue.Front());
     }
 }
